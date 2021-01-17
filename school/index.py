906c19d2df981f4fe25507a704d127b928680063
siyang.gu <siyang.gu@1mu.club>
2021-01-17 10:49:45 +0800

    20210117

--- school/index.py
+++ school/index.py
@@ -4,15 +4,66 @@ from fastapi.templating import Jinja2Templates
 
 # uvicorn index:app --reload    启动命令
 
-app =fastapi.FastAPI()
+app = fastapi.FastAPI()
 databases = databases.Database("sqlite:///../sql/schoolDb.db")
 templates = Jinja2Templates(directory="templates")
-@app.get("/",response_class=fastapi.responses.HTMLResponse)
-async def root(request:fastapi.Request):
+
+
+@app.get("/", response_class=fastapi.responses.HTMLResponse)
+async def root(request: fastapi.Request):
     query = "select 班级名称,学科ID,姓名,成绩 from grade,students,classes where grade.学生ID = students.ID and grade.学科ID = 2 and classes.ID = students.班级ID"
-    rows = await databases.fetch_all(query = query)
-    return templates.TemplateResponse("grade.html",{
-        "request":request,
-        "scores":rows
+    rows = await databases.fetch_all(query=query)
+    return templates.TemplateResponse("index.html", {
+        "request": request,
+        "scores": rows
     })
     # return {"message":"hello world"}
+
+
+@app.get("/math", response_class=fastapi.responses.HTMLResponse)
+async def math(request: fastapi.Request):
+    query = '''
+                SELECT students.ID AS 学生ID,
+                    students.姓名,
+                    g.成绩 AS 数学成绩
+                FROM students
+                    LEFT JOIN
+                    (
+                        SELECT *
+                            FROM grade
+                            WHERE grade.学科ID = 2
+                    )
+                    AS g ON students.ID = g.学生ID;
+    '''
+    rows = await databases.fetch_all(query=query)
+    return templates.TemplateResponse("math.html", {
+        "request": request,
+        "data": rows
+    })
+
+
+@app.get("/editGrade", response_class=fastapi.responses.HTMLResponse)
+async def editGrade(request: fastapi.Request, studentId: int,studentName:str, subjectId: int, grade: float = None):
+    # query = '''
+    #             SELECT students.ID AS 学生ID,
+    #                 students.姓名,
+    #                 g.成绩 AS 数学成绩
+    #             FROM students
+    #                 LEFT JOIN
+    #                 (
+    #                     SELECT *
+    #                         FROM grade
+    #                         WHERE grade.学科ID = 2
+    #                 )
+    #                 AS g ON students.ID = g.学生ID;
+    # '''
+    # rows = await databases.fetch_all(query = query)
+    return templates.TemplateResponse("editGrade.html", {
+        "request": request,
+        "data": {
+            "studentId": studentId,
+            "studentName":studentName,
+            "subjectId": subjectId,
+            "grade": grade,
+        }
+    })
